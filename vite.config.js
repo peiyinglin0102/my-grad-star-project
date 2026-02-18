691aee67ec4b182908be9d4358f37e50d7423b12
peiyinglin0102 <asd1050980406@gmail.com>
2026-02-18 14:40:07 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // 1. 引入插件
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // 2. 加入插件
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/my-grad-star-project/', // 👈 加入這一行，名稱要跟 GitHub 專案一模一樣
 })
